f20be551c0a7f25e19ff21e722475a4e4742251b
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-03-22 09:12:51 +0100

    Hero section demo

---
+++ src/Component/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from "../Images/LOGO (1).png"
+import "../CSS/navbar.css"
+import Button from "./ButtonComponent"
+
+
+function Navbar() {
+  return (
+      <nav className='navbar'>
+          <img className='navbar_image' src={Logo} alt='Logo' />
+          
+            <ul className='navbar_list'>
+              <li className='navbar_listItem'>About</li>
+              <li className='navbar_listItem'>Services</li>
+              <li className='navbar_listItem'>Contact</li>
+          </ul>
+
+          <Button variant="outlined">
+              <span style={{color:"white"}}>Request a demo</span> 
+          </Button>
+         
+        
+      </nav>
+  )
+}
+
+export default Navbar
---
+++ src/Section/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroBody from '../Component/HeroBody'
+import Navbar from '../Component/Navbar'
+
+function Hero() {
+  return (
+      <div>
+          <Navbar />
+          <HeroBody/>
+    </div>
+  )
+}
+
+export default Hero
