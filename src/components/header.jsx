febe512304c84c4b719b1b43941570432e230063
Brain Philemon <philbrainy@gmail.com>
2022-03-24 09:19:46 +0100

    feat: responsive header component

---
+++ src/components/header.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Logo from "../Images/LOGO (1).png";
+import Btx from "../Component/ButtonComponent";
+import DrawerComponent from "../Component/DrawerComponent";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Tabs,
+  Tab,
+  Button,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+const Header = () => {
+  const PAGES = ["About", "Services", "Contact"];
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <React.Fragment>
+      <AppBar>
+        <Toolbar sx={{ backgroundColor: "#081F4A" }}>
+          <img className="navbar_image" src={Logo} alt="Logo" />
+          {isMatch ? (
+            <Box sx={{ marginLeft: "auto" }}>
+              {" "}
+              <DrawerComponent />{" "}
+            </Box>
+          ) : (
+            <Tabs
+              sx={{ marginLeft: "auto" }}
+              textColor="white"
+              value={value}
+              onChange={(e, value) => setValue(value)}
+              indicatorColor="primary"
+            >
+              {PAGES.map((page, index) => 
+              (
+                <Tab
+                  key={index}
+                  sx={{ textTransform: "capitalize", marginLeft: "50px" }}
+                  label={page}
+                ></Tab>
+              )
+              )}
+              <Btx spacing={{ marginLeft: "130px" }} variant="outlined">
+                <span style={{ color: "white", textTransform: "capitalize" }}>
+                  Request a demo
+                </span>
+              </Btx>
+            </Tabs>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
