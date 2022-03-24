8fd63a936a45c508d18b5ade143ce04e6e278535
Brain Philemon <philbrainy@gmail.com>
2022-03-24 09:18:59 +0100

    refactor:  add typescript and code formatting

--- src/Component/HeroBody.js
+++ src/Component/HeroBody.js
@@ -1,35 +1,33 @@
-import React from 'react'
-import "../CSS/herobody.css"
-import aiStyle from "../Images/Vector 7.png"
-import Button from "./ButtonComponent"
+import React from "react";
+import "../CSS/herobody.css";
+import aiStyle from "../Images/Vector 7.png";
+import Button from "./ButtonComponent";
 
 function HeroBody() {
-    return (
-      <div className='hero'>
+  return (
+    <div className="hero">
       <div className="hero_image">
-        
-            <div className="hero-text">
-                    <h1 style={{ fontSize: "50px" }}>Tell Us The Problem <br /> We'll solve it with   
-                        <span  style={{ color: "yellow"}}>
-                            &nbsp; A.I
-                        </span>
-                        <span className='hero_span'>
-                                 <img src={aiStyle} alt="ai Style" />
-                            </span>
-                        
-                    </h1>
-                    <p style={{ paddingBottom: "30px" }}>Lets give your business a head start!</p>
-                    
-                    <Button spacing={{ marginRight: "2rem" }} variant="contained">
-                        <span style={{color:"black"}}>Request a Demo</span>
-                    </Button>
-                 
-                     <Button variant="outlined">Experiment A.I</Button>
-                </div>
-                  
+        <div className="hero-text">
+          <h1 style={{ fontSize: "50px" }}>
+            Tell Us The Problem <br /> We'll solve it with
+            <span style={{ color: "yellow" }}>&nbsp; A.I</span>
+            <span className="hero_span">
+              <img src={aiStyle} alt="ai Style" />
+            </span>
+          </h1>
+          <p style={{ paddingBottom: "30px" }}>
+            Lets give your business a head start!
+          </p>
+
+          <Button spacing={{ marginRight: "2rem" }} variant="contained">
+            <span style={{ color: "black" }}>Request a Demo</span>
+          </Button>
+
+          <Button variant="outlined">Experiment A.I</Button>
         </div>
-</div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default HeroBody
+export default HeroBody;
