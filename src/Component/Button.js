b065f195e1298abf6908ed4f277e92a8d2cf9270
Joshua Oyewole <joshua.oyewole@zummitlabs.com>
2022-03-28 17:13:03 +0100

    added default parameter to the margin props in button component

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Button } from '@mui/material'
 
 
-export default function PrimaryButton({children, bgcolor, color,isDesktop}) {
+export default function PrimaryButton({children, bgcolor, color,isDesktop, margin="5rem 0 0 0"}) {
   // Media Query to be applied on based on Screen Size
-  let screenSize = isDesktop ? "5rem 0 0 0" : "2rem auto";
+  let screenSize = isDesktop ? `${margin}` : "2rem auto";
   
   return (
     <Button variant="contained" display='flex' size= 'large' sx={{backgroundColor:`${bgcolor}`, color:`${color}`, display: 'flex', m: `${screenSize}`, "&:hover": {
