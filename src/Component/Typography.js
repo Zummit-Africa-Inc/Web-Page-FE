3783eb43bb80156e57314d07b45dda3941628509
Joshua Oyewole <joshua.oyewole@zummitlabs.com>
2022-04-04 23:00:05 +0100

    Rectified responsiveness bgs for Header and sub header text

--- src/Component/Typography.js
+++ src/Component/Typography.js
@@ -11,9 +11,9 @@ export function PrimaryHeader({children, pbSize, fontSize='2. 125rem'}) {
   )
 }
 
-export function Header({children, textSize, textAlign}) {
+export function Header({children, textSize, textAlign, pady='1rem 0rem 1rem 0rem'}) {
     return (
-      <Typography variant={textSize} component={textSize} sx={{py: '1rem', textAlign: {textAlign}}} >
+      <Typography variant={textSize} component={textSize} sx={{p: `${pady}`, textAlign: {textAlign}}} >
           {children}
       </Typography>
     )
