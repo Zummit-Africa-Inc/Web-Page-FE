14d6a511de01ce74fc08094e71d28b7dc33f8148
Joshua Oyewole <joshua.oyewole@zummitlabs.com>
2022-04-05 20:22:41 +0100

    removed all the placeholders in the input fields

--- src/Section/AIinAction.js
+++ src/Section/AIinAction.js
@@ -27,18 +27,18 @@ export default function AIinAction() {
         <Grid item xs={12}  md={6}>
         <Box style={style.formControl}>
             <label htmlFor="company_name" style={style.label}>Enter body text / paragraph</label>
-            <TextField id="filled-basic" label="Body text" variant="filled" fullWidth multiline rows="3" style={{backgroundColor: 'white'}}/>
+            <TextField id="filled-basic" fullWidth multiline rows="3.65" style={{backgroundColor: 'white'}}/>
         </Box>
         <Box style={style.formControl}>
             <label htmlFor="fullnames" style={style.label}>Enter Question</label>
-            <TextField id="filled-basic" label="Question" variant="filled" fullWidth style={{backgroundColor: 'white'}} size="small" />
+            <TextField id="filled-basic" fullWidth style={{backgroundColor: 'white'}} size="small" />
         </Box>
         <PrimaryButton color= '#09090B' bgcolor='#FFEA00' margin="2rem 0" isDesktop={isDesktop}>Generate answer</PrimaryButton> 
         </Grid>
         <Grid item xs={12}  md={6}>
         <Box style={style.formControl}>
             <label htmlFor="company_name" style={style.label}>Answer</label>
-            <TextField id="filled-basic" label="Answer" variant="filled" fullWidth multiline rows="7.5" style={{backgroundColor: 'white'}}/>
+            <TextField id="filled-basic" fullWidth multiline rows="7.5" style={{backgroundColor: 'white'}}/>
         </Box>
         </Grid>
     </Grid>
