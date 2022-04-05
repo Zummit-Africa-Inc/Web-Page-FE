a2365d7451e1346763e4601db722a2a49652ff06
Zeemari <Zenntia@gmail.com>
2022-04-05 15:37:46 +0100

    final commit

--- src/Section/FooterSection.js
+++ src/Section/FooterSection.js
@@ -4,10 +4,10 @@ import { Link } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import React from 'react'
-import Logo from '../Images/LOGO (1).png'
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Logo from '../Images/LOGO.png'
+import Facebook from '../Images/Facebook.png'
+import Twitter from '../Images/Twitter.png'
+import Linkedln from '../Images/Linkedln.png';
 //Material UI icons
 export default function FooterSection() {
     const theme = useTheme();
@@ -22,6 +22,7 @@ export default function FooterSection() {
 // Breakpoint padding
 
     return (
+        
         <Box container
             component="footer"
             sx={{ py: "4rem", backgroundColor: "#FFFFFF", color: "#081F4A" }}
@@ -30,13 +31,11 @@ export default function FooterSection() {
             <Grid container Spacingcolumn={6} Spacingrow={5}>
                 <Grid item xs={6} lg={4} order={contactOrder} mt={{xs: 3, sm: 4, md: 6, lg: 0}} >
                     <img src={Logo} alt="Zummit Africa Logo" /> 
-                    <Typography display="block">256 Chapman Road STE <Typography>
-                        105-4, Newark. Newcastle</Typography>
-                        Delaware, USA.</Typography>
-                    <Typography display="block">TOJA Southern View Estate,</Typography>
-                        <Typography display="block">Lekki - Lagos, Nigeria.</Typography>
-                    <Typography display="block">contact@zummitafrica.com
-                        <Typography>+234 816 3816 789</Typography>
+                    <Typography display="block" py={1} fontSize={16} fontWeight={500} >256 Chapman Road STE <br/> 105-4, Newark. Newcastle <br/> Delaware, USA.</Typography>
+                    <Typography display="block" py={1} fontSize={16} fontWeight={500}>TOJA Southern View Estate, <br/> Lekki - Lagos, Nigeria
+                    </Typography>
+                        
+                    <Typography display="block" fontSize={16} fontWeight={500}>contact@zummitafrica.com <br/>+234 816 3816 789
                     </Typography>
                 </Grid>
                 <Grid item xs={6} lg={2} order={productOrder} >
@@ -44,22 +43,22 @@ export default function FooterSection() {
                         Products
                     </Typography>
                     <Link href="/Apis" underline="none" color="#081F4A">
-                        <Typography display="block">AI APIs</Typography>
+                        <Typography display="block" py={1} fontSize={14}  fontWeight={500}>AI APIs</Typography>
                     </Link>
                     <Link href="/Detect" underline="none" color="#081F4A">
-                        <Typography display="block">Emotion Detection</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Emotion Detection</Typography>
                     </Link>
                     <Link href="/drows" underline="none" color="#081F4A">
-                        <Typography display="block">Drowsiness Detection</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Drowsiness Detection</Typography>
                     </Link>
                     <Link href="/chat" underline="none" color="#081F4A">
-                        <Typography display="block">Chat bots</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Chat bots</Typography>
                     </Link>
                     <Link href="/Face" underline="none" color="#081F4A">
-                        <Typography display="block">Face recognition</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Face recognition</Typography>
                     </Link>
                     <Link href="/sentiment" underline="none" color="#081F4A">
-                        <Typography display="block">Sentiment analysis</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Sentiment analysis</Typography>
                     </Link>
                 </Grid>
                 <Grid item xs={6} lg={2}  pl={{ xs: '2rem', sm: 4, md: 6, lg:'2rem' }} order={companyOrder}>
@@ -67,16 +66,16 @@ export default function FooterSection() {
                         Company
                     </Typography>
                     <Link href="/About" underline="none" color="#081F4A">
-                        <Typography display="block">About</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>About</Typography>
                     </Link>
                     <Link href="/Service" underline="none" color="#081F4A">
-                        <Typography display="block">Services</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Services</Typography>
                     </Link>
                     <Link href="/Internship" underline="none" color="#081F4A">
-                        <Typography display="block">Internship</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Internship</Typography>
                     </Link>
                     <Link href="/Contact" underline="none" color="#081F4A">
-                        <Typography display="block">Contact</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Contact</Typography>
                     </Link>
                 </Grid>
                 <Grid item xs={6} lg={2}   mt={{ xs: '2rem', sm: 4, md: 6, lg: 0}} order={resourcesOrder}>
@@ -84,10 +83,10 @@ export default function FooterSection() {
                         Resources
                     </Typography>
                     <Link href="/Blog" underline="none" color="#081F4A">
-                        <Typography display="block">Blog</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Blog</Typography>
                     </Link>
                     <Link href="/Customer" underline="none" color="#081F4A">
-                        <Typography display="block">Customers</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Customers</Typography>
                     </Link>
                 </Grid>
                 <Grid item xs={6} lg={2}  mt={{ xs: '2rem', sm: 4, md: 6, lg: 0}} pl={{ xs: '2rem', sm: 4, md: 6,lg:0 }} order={legalOrder}>
@@ -96,25 +95,25 @@ export default function FooterSection() {
                     </Typography>
                     <Link href="/Terms" underline="none" color="#081F4A">
                     
-                        <Typography display="block">Terms</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Terms</Typography>
                        
                     </Link>
                     <Link href="/Privacy" underline="none" color="#081F4A">
-                        <Typography display="block">Privacy</Typography>
+                        <Typography display="block"py={1} fontSize={14}  fontWeight={500}>Privacy</Typography>
                     </Link>
                 </Grid>
 
             </Grid>
             <Grid container mt={{ xs: '2rem', sm: 4, md: 6, lg: 4}} >
-                <Grid item xs={12} lg={6} >
+                <Grid item xs={12} lg={6} fontSize={10} >
                     <Box mt={4} mr={{ xs: '1rem', lg: '1rem'}} display='inline-block'>
-                        <FacebookIcon />
+                    <img src={Twitter} alt="Twitter" /> 
                     </Box>
                     <Box mt={4} mr={{ xs: '1rem', lg: '1rem'}} display='inline-block'>
-                        <TwitterIcon />
+                    <img src={Facebook} alt="Facebook" /> 
                     </Box>
                     <Box mt={4} mr={{ xs: '1rem', lg: '1rem'}} display='inline-block'>
-                        <LinkedInIcon />
+                    <img src={Linkedln} alt="Linkedln" /> 
                     </Box>
                     
                    
@@ -122,7 +121,7 @@ export default function FooterSection() {
 
                 </Grid>
                 
-                <Grid item xs={12} mt={{ xs: '1rem'}} lg={6} display="flex" justifyContent= {isMobile ? "flex-end":"flex-start"} order={vectorOrder} 
+                <Grid item xs={12} mt={{ xs: '1rem'}} lg={6} display="flex" fontSize={14} fontWeight={500} justifyContent= {isMobile ? "flex-end":"flex-start"} order={vectorOrder} 
                 pr={isMobile ? '7rem' : '0'}>
                    &copy; 2022, Zummit Africa.
                 </Grid>
