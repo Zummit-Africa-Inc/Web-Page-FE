0e356202f976cf3a7d8a216f0c2da57e04b073ed
Samson Okunola <smsnmicheal@gmail.com>
2022-07-28 21:17:44 +0100

    fixed why zummit section styling

--- src/Section/AcadWhyZummit.jsx
+++ src/Section/AcadWhyZummit.jsx
@@ -8,8 +8,8 @@ import { exampleText2 } from '../pages/example'
 const useStyles = makeStyles({
   whyZummit: {
     width: '100%',
-    padding: '1rem 3rem',
-    backgroundColor: 'var(--live-bg)',
+    padding: '3.5rem 3rem',
+    backgroundColor: "rgba(236, 242, 253, 0.4)",
   },
 })
 
@@ -18,15 +18,11 @@ const AcadWhyZummit = () => {
 
   return (
     <Stack className={classes.whyZummit}>
-        <Typography variant='subtitle1' fontSize={16} fontWeight={400} color="text.primary" my={2} textTransform='uppercase'>
-          What makes us unique
-        </Typography>
-
         <Typography variant='h2' color="text.primary" fontSize={64} fontWeight={700} my={2} textTransform='capitalize'>
           Why learn with Zummit Africa?
         </Typography>
 
-        <Typography variant='body1' fontSize={20} fontWeight={400} color="text.secondary" mt={4} mb={12}>
+        <Typography variant='body1' fontSize={20} fontWeight={400} color="text.secondary" mt={4} mb={8}>
           Our data science course is designed to help you learn-by-doing with live classes <br />
           taught by an industry expert, making you ready for work as a Data Scientist.
         </Typography>
