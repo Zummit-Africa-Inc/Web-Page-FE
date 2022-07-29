eadd4008b649df0918bac38fba45ad854515fabb
Okunola Samson <68032187+pablo-clueless@users.noreply.github.com>
2022-07-29 19:51:01 +0100

    Update AcaSecTwo.jsx

--- src/Section/AcaSecTwo.jsx
+++ src/Section/AcaSecTwo.jsx
@@ -33,7 +33,7 @@ function AcaSecTwo() {
           sx={{
             fontSize: '20px'
           }}
-          color="text.secondary"
+          color="#5B6773"
         >
           Our data science course is designed to help you learn-by-doing with
           live classes <br /> taught by an industry expert, making you ready for
